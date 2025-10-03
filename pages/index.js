5563ef143f64921734dd945af051b7f1147f209a
balianima2025v1-afk <balianima2025v1@gmail.com>
2025-10-03 14:06:05 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World 🚀</h1>
+      <p>CI/CD with Vercel is running!</p>
+    </div>
+  );
+}
